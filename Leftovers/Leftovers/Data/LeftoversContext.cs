f74b5f1200fb60490da7581f49db5cf76cfab994
IevLik <79166922+IevLik@users.noreply.github.com>
2022-11-18 00:02:23 +0200

    up

--- Leftovers/Leftovers/Data/LeftoversContext.cs
+++ Leftovers/Leftovers/Data/LeftoversContext.cs
@@ -16,10 +16,10 @@ namespace Leftovers.Data
         public DbSet<Meal> Meals { get; set; }
 
         private readonly IConfiguration _configuration;
-        public LeftoversContext(IConfiguration configuration)
+        /*public LeftoversContext(IConfiguration configuration)
         {
             _configuration = configuration; 
-        }
+        }*/
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseNpgsql(_configuration.GetValue<string>("PostgreSQLConnectionString"));//("Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog=Leftovers")    -   UseNpgsql(_configuration.GetValue<string>("PostgreSQLConnectionString"))
